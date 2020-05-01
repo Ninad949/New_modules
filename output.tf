3e9919c14a7255dd8236c7c00178fa17e630394c
root <root@ip-172-31-12-188.us-west-1.compute.internal>
2020-05-01 05:54:20 +0000

    new54

--- output.tf
+++ output.tf
@@ -12,12 +12,19 @@ output "private_table_id" {
   value = "${aws_route_table.private_table.id}"
 }
 
+#output "aws_gateway_id "  {
+
+#value = "${aws_internet_gateway.main.id}"
+
+#}
+
+
 
 output "public_subnet_id" {
 
 #type = list(string)
 
-value = "${aws_subnet.public_subnets.public.id}"
+value = "${aws_subnet.public_subnets.id}"
 
 }
 
@@ -25,17 +32,9 @@ value = "${aws_subnet.public_subnets.public.id}"
 output "private_subnet_id" {
 
 #type = list(string)
-value = "${aws_subnet.private_subnets.private.id}"
+value = "${aws_subnet.private_subnets.id}"
 
 }
-
-
-#output "internet__id "  {
-
-#value = "${aws_internet_gateway.main.id}"
-
-#}
-
 
 
 
