f8fb20263033fb9c143c1d160c590dc4a3964dfb
root <root@ip-172-31-12-188.us-west-1.compute.internal>
2020-05-01 05:13:18 +0000

    New23

--- main.tf
+++ main.tf
@@ -5,16 +5,25 @@ provider "aws" {
 
 module "my_vpc" {
   
-    source= "/var/terraform2/new_terraform/modules"
+    source= "/home/modules"
   
     vpc_cidr = "10.0.0.0/16"
 
   tenancy = "default"
 
-#  vpc_id ="${ module.vpc_module.vpc_id}"
+  vpc_id ="${ module.my_vpc.vpc_id}"
 
   count_private_subnet = 2
 
   count_public_subnet = 2
+
+private_subnets = "${module.my_vpc.private_subnets}"
+
+public_subnets = "${module.my_vpc.public_subnets}"
+
+#public_subnet_id = "${module.my_vpc.public_subnet_id}"
+
+# private_subnet_id = "${module.my_vpc.private_subnet_id}"
+
 }
 
